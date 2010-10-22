6834c82b7608ffd9622f64e6cc1b9fdcbeecffcb
John Hobbs <john@velvetcache.org>
2010-10-22 16:02:20 -0500

    Made the title safe in the mobile template.

--- application/views/mobile.php
+++ application/views/mobile.php
@@ -1,13 +1,13 @@
 <html>
 	<head>
-		<title><?php echo $title; ?></title>
+		<title><?php echo HTML::chars( $title ); ?></title>
 		<!--// http://joehewitt.com/files/iphone/navigation.html //-->
 		<meta name="viewport" content="width=device-width,user-scalable=no" />
 		<link rel="apple-touch-icon" href="<?php echo URL::site( 'image/iphone.png' ); ?>" />
 		<?php echo HTML::style( 'style/iphone/iphone.css' ); ?>
 	</head>
 	<body>
-		<h1 id="pageTitle"><?php echo $title; ?></h1>
+		<h1 id="pageTitle"><?php echo HTML::chars( $title ); ?></h1>
 		<?php
 			if( is_array( $left ) ) {
 				echo HTML::anchor( $left['target'], HTML::chars( $left['text'] ), array( 'class' => 'button', 'id' => 'homeButton' ) );
@@ -17,6 +17,7 @@
 			}
 		?>
 		<div class="panel">
+			<?php echo Message::display(); ?>
 			<?php echo $content; ?>
 		</div>
 	</body>
